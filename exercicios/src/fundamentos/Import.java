9087d20bcbeeb466b0daf7fe44cd89a7ef0a5e00
Antonio Carlos Tavares <123905174+TavCarlos@users.noreply.github.com>
2023-04-22 20:02:42 -0300

    Add files via upload

---
+++ exercicios/src/fundamentos/Import.java
@@ -0,0 +1,15 @@
+package fundamentos;
+//Esse tema será apronfundado em outro momento!
+//Nem todas as funcionalidades estão disponíveis por padrão, sendo necessário importa-las do proprio JAVA.
+import java.util.Date;
+
+public class Import {
+
+	public static void main(String[] args) {
+
+		
+		Date date = new Date();
+		System.out.println(date);
+		//Utilizando o CTRL+Espaço ao escrever o ''Date'' a própria IDE ajuda no import.
+	}
+}
