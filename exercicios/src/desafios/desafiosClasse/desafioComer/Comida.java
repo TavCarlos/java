c68ec938c6f9936a352c669f2ccbab2a29dd9c55
Antonio Carlos Tavares <123905174+TavCarlos@users.noreply.github.com>
2023-05-09 20:25:19 -0300

    Add files via upload

---
+++ exercicios/src/desafios/desafiosClasse/desafioComer/Comida.java
@@ -0,0 +1,12 @@
+package desafios.desafiosClasse.desafioComer;
+
+public class Comida {
+
+	String NomeComida;
+	double PesoComida;
+	
+	Comida(String NomeComida, double PesoComida){
+		this.NomeComida = NomeComida;
+		this.PesoComida = PesoComida;
+	}
+}
