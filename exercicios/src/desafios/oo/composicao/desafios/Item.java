46f0c929a33ac7715f97f8b0ac7363219a7ecdd4
Antonio Carlos Tavares <123905174+TavCarlos@users.noreply.github.com>
2023-05-16 19:14:43 -0300

    Add files via upload

---
+++ exercicios/src/desafios/oo/composicao/desafios/Item.java
@@ -0,0 +1,13 @@
+package desafios.oo.composicao.desafios;
+
+public class Item {
+
+	int quantidade;
+	Produto produto;
+	
+	
+	Item(int quantidade, Produto produto){
+		this.quantidade = quantidade;
+		this.produto = produto;
+	}
+}
