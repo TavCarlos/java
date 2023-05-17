0ab751471f03f0b67b885069117be26318d3495f
Antonio Carlos Tavares <123905174+TavCarlos@users.noreply.github.com>
2023-05-17 19:28:12 -0300

    Add files via upload

---
+++ exercicios/src/desafios/oo/composicao/desafios/parte1/Produto.java
@@ -0,0 +1,12 @@
+package desafios.oo.composicao.desafios.parte1;
+
+public class Produto {
+
+	String nome;
+	double preco;
+	
+	Produto(String nome, double preco){
+		this.nome = nome;
+		this.preco = preco;
+	}
+}
