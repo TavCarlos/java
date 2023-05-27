59e94463c635be86b7d047f0404371fe551bc0e1
Antonio Carlos Tavares <123905174+TavCarlos@users.noreply.github.com>
2023-05-27 19:31:10 -0300

    Add files via upload

---
+++ exercicios/src/desafios/desafiosStreams/Car.java
@@ -0,0 +1,15 @@
+package desafios.desafiosStreams;
+
+public class Car {
+
+	final String nome;
+	double price;
+	final boolean isNacional;
+	
+	public Car(String nome, double price, boolean isNacional) {
+		this.nome = nome;
+		this.price = price;
+		this.isNacional = isNacional;
+	}
+	
+}
